40e4459918c5812d235e35cb66c7b4fae82e2951
Erik Kamalov <ekamalov967@gmail.com>
2019-10-15 19:09:29 +0600

    fixed Bundle bug

--- Example/Controllers/Authorization.swift
+++ Example/Controllers/Authorization.swift
@@ -28,7 +28,7 @@ class Authorization: UIViewController {
         self.view.backgroundColor = .black
         
         wrapperView.layout { $0.left.right.margin(6.7%).height(260).centerY() }
-        logo.layout { $0.bottom(of: wrapperView, aligned: .top, 20, relation: .equal).size(width: 250, height: 150).centerX() }
+        logo.layout { $0.bottom(of: wrapperView, 20, aligned: .top).size(width: 250, height: 150).centerX() }
         
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
